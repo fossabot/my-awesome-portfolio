57e1032eef2af19b339152262f55c0bc96de9e3b
Thomas Groch <thomas.groch@gmail.com>
2019-04-01 21:31:02 -0300

    vee-validate translated

--- portfolio/.vuepress/enhanceApp.js
+++ portfolio/.vuepress/enhanceApp.js
@@ -1,11 +1,21 @@
-import VeeValidate from 'vee-validate';
-
+import VeeValidate, {Validator} from 'vee-validate'
+import locale from './vee-validate-locales/pt_BR'
+5
 export default ({
-                  Vue, // the version of Vue being used in the VuePress app
-                  options, // the options for the root Vue instance
-                  router, // the router instance for the app
-                  siteData // site metadata
-                }) => {
-  // ...apply enhancements to the app
-  Vue.use(VeeValidate)
+					Vue, // the version of Vue being used in the VuePress app
+					options, // the options for the root Vue instance
+					router, // the router instance for the app
+					siteData // site metadata
+				}) => {
+	// ...apply enhancements to the app
+	Vue.use(VeeValidate)
+	// Vue.mixin({
+	// 	localize (localeName) {
+	// 		// asynchronously load the locale file then localize the validator with it.
+	// 		import(`./vee-validate-locales/${localeName}`).then(locale => {
+	// 			Validator.localize(localeName, locale);
+	// 		});
+	// 	}
+	// })
+	Validator.localize('pt_BR', locale)
 }
